0fb1285a0c28b1819a4ecb30111ae35523b75fcb
Hairul Ardi <hairul.ardi@hotmail.com>
2017-06-15 15:44:12 +0700

    all page error

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Service Unavailable (503)</title>
+    <style>
+        body{
+            font-family: "calibri";
+            text-align: center;
+        }
+        h1{
+            font-size: 40px;
+            color: #C75C5C;
+        }
+        img{
+            max-width: 350px;
+            height: auto;
+            margin: 35px 20px 20px;
+        }
+        p.notif{
+            font-size: 17px;
+        }
+    </style>
+</head>
+<body>
+    <img src="{{ asset('img/stop.svg') }}" alt="Forbidden Access">
+    <h1>ACCESS DENIED</h1>
+    <p class="notif">ANDA TIDAK PUNYA AKSES KE HALAMAN INI</p>
+    <p>Sinkronisasi Laporan Keuangan Kabupaten Biak. Develop By: <a href="" target="blank">Bangsa Cerdas</a></p>
+</body>
+</html>
